08ce6ee2d5e8b9ee1f4bcde086172365f9ace8cb
engjoao <aquinosilva@gmail.com>
2016-09-14 19:28:53 -0300

    Add User ID to Messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914190103) do
+ActiveRecord::Schema.define(version: 20160914221046) do
 
   create_table "messages", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
